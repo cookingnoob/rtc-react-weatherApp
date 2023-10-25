b246fbe2c06b9e425789ba53670fc7560b106903
cookingnoob <costealan@gmail.com>
2023-10-25 12:36:50 -0600

    add map to render the data of the next 5 days

--- src/components/FiveDaysWeather.jsx
+++ src/components/FiveDaysWeather.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
 
 const FiveDaysWeather = () => {
+  const API_KEY = '5a8c226189094d71c9d4cdd8e366f881';
   const [localWeatherData, setLocalWeatherData] = useState(null);
   const [texto, setTexto] = useState('Obteniendo tu ubicación');
-  const API_KEY = '5a8c226189094d71c9d4cdd8e366f881';
+
 
   useEffect(() => {
 
@@ -28,10 +29,20 @@ const FiveDaysWeather = () => {
       navigator.geolocation.getCurrentPosition(success, error);
     }
 
+    // const whatDayIsIt = new Date('2023-10-25').toLocaleDateString('es-ES', { weekday: 'long' })
+    // console.log(whatDayIsIt)
   }, [])
   return (
     <>
-      <h1>{texto}</h1>
+      {localWeatherData ?
+        localWeatherData.list.map(day => (
+          <div key={day.dt}>
+            <p>{day.main.temp}ºC, {day.weather[0].description} <span><img src={`http://openweathermap.org/img/w/${day.weather[0].icon}.png`} alt={day.weather[0].description} /></span></p>
+            <p>{day.dt_txt}</p>
+          </div>
+        ))
+        : <p>cargando elementos || {texto}</p>
+      }
     </>
   )
 }
@@ -40,6 +51,22 @@ export default FiveDaysWeather
 
 
 //datos para el mock:
+//data.city
 //sunrise : 1698150841
 // sunset : 1698192465
 // timezone : -21600
+//data.list
+//dt : 1698202800
+//dt_txt : "2023-10-25 03:00:00"
+//main
+//temp : 20.32
+//weather[0]
+//description : "light rain"
+//icon : "10n"
+//id : 500
+//main : "Rain"
+//dt_txt : "2023-10-26 03:00:00"
+//data.list.dt_text dt_txt: "2023-10-27 15:00:00"
+//dt_txt : "2023-10-28 03:00:00"
+
+
