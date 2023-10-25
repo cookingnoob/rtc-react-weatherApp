80859d6360fa50a6c977b47da225f5215d8b7c18
cookingnoob <costealan@gmail.com>
2023-10-24 18:04:32 -0600

    deleted couple of useState on GetLocation to avoid an extra render

--- src/components/GetLocation.jsx
+++ src/components/GetLocation.jsx
@@ -5,16 +5,14 @@ import ActualWeather from './ActualWeather';
 const GetLocation = () => {
 
   const [localWeatherData, setLocalWeatherData] = useState(null);
-  const [texto, setTexto] = useState('');
+  const [texto, setTexto] = useState('Obteniendo tu ubicación');
   const API_KEY = '5a8c226189094d71c9d4cdd8e366f881';
 
   useEffect(() => {
 
     async function success(position) {
-      const latitude = position.coords.latitude;
-      const longitude = position.coords.longitude;
       try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${API_KEY}&units=metric`);
         const data = await response.json()
         setLocalWeatherData(data)
       } catch {
@@ -31,7 +29,6 @@ const GetLocation = () => {
     if (!navigator.geolocation) {
       setTexto('Tu navegador no tiene soporte para la geolocalización')
     } else {
-      setTexto('Obteniendo tu ubicación...')
       navigator.geolocation.getCurrentPosition(success, error);
     }
 
@@ -44,6 +41,7 @@ const GetLocation = () => {
             <h1>{Math.round(localWeatherData.main.temp)}ºC</h1>
             <h2>{localWeatherData.sys.country}: {localWeatherData.name}
               <span><img src={`http://openweathermap.org/img/w/${localWeatherData.weather[0].icon}.png`} alt={localWeatherData.weather[0].description} /></span>
+              <span><p>{localWeatherData.weather[0].description}</p></span>
             </h2>
           </div>
           :
