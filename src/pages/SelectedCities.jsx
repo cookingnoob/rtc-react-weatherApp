fca57e905e21c3079f485f9bf163d223cb16b3b6
cookingnoob <costealan@gmail.com>
2023-11-03 09:57:12 -0600

    added Tokyo to the selected cities

--- src/pages/SelectedCities.jsx
+++ src/pages/SelectedCities.jsx
@@ -27,6 +27,7 @@ const SelectedCities = () => {
         <option value="Paris">París</option>
         <option value="Berlin">Berlín</option>
         <option value="London">Londres</option>
+        <option value="Tokyo">Tokyo</option>
       </select>
       {latitude && longitude ?
         <>
