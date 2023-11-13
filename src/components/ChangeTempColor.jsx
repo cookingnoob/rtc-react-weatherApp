075eb75a097774768dbd9a5e2de77626cff26e80
alan <alan@iMac-de-alan.local>
2023-11-13 08:42:22 -0600

    added images to the ChangeTempColor, also a useEffect and useState to change the image according to the weather conditions

--- src/components/ChangeTempColor.jsx
+++ src/components/ChangeTempColor.jsx
@@ -1,12 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../App.css'
+import planeta from '../assets/foto-default.png'
+import soleado from '../assets/dia-soleado.png'
 import lluvia from '../assets/dia-lluvio.png'
+import nublado from '../assets/dia-nublado.png'
+import tormenta from '../assets/tormenta.png'
+import neblina from '../assets/neblina.png'
+import nieve from '../assets/nieve.png'
+
 
 const ChangeTempColor = ({ weather }) => {
+  const [image, setImage] = useState(planeta);
 
+  useEffect(() => {
+  if(weather == 'Clear'){
+    setImage(soleado)
+  } else if(weather.includes('Rain')){
+    setImage(lluvia)
+  } else if(weather.includes('Clouds')){
+    setImage(nublado)
+  } else if(weather == 'Thunderstorm'){
+    setImage(tormenta)
+  } else if(weather == 'Mist'){
+    setImage(neblina)
+  } else if (weather == 'Snow'){
+    setImage(nieve)
+  } else {
+    console.log('no funciono, el clima es:', weather)
+  }
+}, [weather]);
 
-
-  return <img className='main-image' src={lluvia}></img>
+  return <img className='main-image' src={image}></img>
 };
 
 export default ChangeTempColor
