392a732e36f5b61d9586803e9801e1afe2978be2
cookingnoob <costealan@gmail.com>
2023-11-15 13:00:42 -0600

    created the useSelectLocation hook to separate the logic to get the longitude and latitude according to a city name

---
+++ src/hooks/useSelectLocation.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react'
+
+const useSelectLocation = ({ city }) => {
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null)
+
+  useEffect(() => {
+    const getCityLocation = async () => {
+      const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=5a8c226189094d71c9d4cdd8e366f881&units=metric&appid=5a8c226189094d71c9d4cdd8e366f881&units=metric`)
+      const data = await response.json()
+      setLatitude(data[0].lat)
+      setLongitude(data[0].lon)
+    }
+    getCityLocation()
+  }, [city])
+
+  return (
+    { latitude, longitude }
+  )
+}
+
+export default useSelectLocation
