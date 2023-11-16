5afcefac4cbadb70154c5a7b79b5b439f821a165
alan <alan@iMac-de-alan.local>
2023-11-16 12:52:15 -0600

    added sunset data to ActualLocalWeater component

--- src/components/ActualLocalWeather.jsx
+++ src/components/ActualLocalWeather.jsx
@@ -3,11 +3,12 @@ import ChangeTempColor from './ChangeTempColor'
 
 const ActualLocalWeather = ({ latitude, longitude }) => {
   const { weatherData, texto } = useAPI({ url: 'https://api.openweathermap.org/data/2.5/weather?', latitude, longitude })
-  
-  let sunRise;
 
-  weatherData ?  sunRise = new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString() : console.log('hola')
+  let sunRise;
+  weatherData ?  sunRise = new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString().slice(0, 5) : 'No se pudo obtener la información'
   
+  let sunSet;
+  weatherData ?  sunSet = new Date(weatherData.sys.sunset * 1000).toLocaleTimeString().slice(0, 5) : 'No se pudo obtener la información'
   return (
     <div className='mainPage'>
       {weatherData ?
@@ -15,7 +16,8 @@ const ActualLocalWeather = ({ latitude, longitude }) => {
           <h1>{Math.round(weatherData.main.temp)}</h1>
           <h2>{weatherData.sys.country}: {weatherData.name}</h2>
           <div className="extra-data">
-            <p>Amanece: {sunRise.slice(0, 5)}</p>
+            <p>Amanece: {sunRise}</p>
+            <p>Atardecer: {sunSet}</p>
           </div>
           <ChangeTempColor weather={weatherData.weather[0].main} />
         </>
